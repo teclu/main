f7f02f7462288ea0a6c1a88d886d7f8c320c101a
Stanley Tay <saintpatricktay@gmail.com>
2017-10-08 23:04:03 +0800

    Fixed checkstyle errors detected by Travis CI.

--- src/main/java/seedu/address/ui/PersonPanel.java
+++ src/main/java/seedu/address/ui/PersonPanel.java
@@ -3,14 +3,15 @@ package seedu.address.ui;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
+
 import javafx.fxml.FXML;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.events.ui.PersonPanelSelectionChangedEvent;
-import seedu.address.model.person.ReadOnlyPerson;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.FlowPane;
+import seedu.address.model.person.ReadOnlyPerson;
 
 /**
  * The Person Panel of the App, which displays the contact details of a selected person.
@@ -37,6 +38,9 @@ public class PersonPanel extends UiPart<Region> {
         registerAsAnEventHandler(this);
     }
 
+    /**
+     * Loads the person's contact details.
+     */
     private void loadPersonPage(ReadOnlyPerson person) {
         name.setText(person.getName().fullName);
         phone.setText("Phone: " + person.getPhone().toString());
