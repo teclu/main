d9e1b01899563e4164cdf8ca6a3bda0837004487
KEVIN\Kevin <kevin2leona@gmail.com>
2017-10-10 22:34:48 +0800

    Model : Create class that implements Predicate to test Tag Lists

---
+++ src/main/java/seedu/address/model/person/TagListContainsKeywordsPredicate.java
@@ -0,0 +1,14 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.tag.Tag;
+    
+/**
+ * Tests that a {@code ReadOnlyPerson}'s {@code UniqueTagList} has a {@code Tag} that matches any of the keywords given.
+ */
+public class TagListContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> {
+
+}
