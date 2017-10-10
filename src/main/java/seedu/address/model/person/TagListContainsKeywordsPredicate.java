0336267aea8927edcf669151d9a0b22be3dd7aa2
KEVIN\Kevin <kevin2leona@gmail.com>
2017-10-10 22:37:18 +0800

    Implement instantiation method for tag list predicate class

--- src/main/java/seedu/address/model/person/TagListContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/TagListContainsKeywordsPredicate.java
@@ -10,5 +10,11 @@ import seedu.address.model.tag.Tag;
  * Tests that a {@code ReadOnlyPerson}'s {@code UniqueTagList} has a {@code Tag} that matches any of the keywords given.
  */
 public class TagListContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> {
+    private final List<String> keywords;
+
+    public TagListContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
 
 }
