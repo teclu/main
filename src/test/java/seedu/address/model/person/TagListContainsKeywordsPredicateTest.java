33a95a70e1dc43ac290eae740bec0a5f8bf39ca4
KEVIN\Kevin <kevin2leona@gmail.com>
2017-10-15 03:43:08 +0800

    Add test for equals method

--- src/test/java/seedu/address/model/person/TagListContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/TagListContainsKeywordsPredicateTest.java
@@ -1,4 +1,41 @@
 package seedu.address.model.person;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class TagListContainsKeywordsPredicateTest {
+
+    @Test
+    public void equals() {
+        List<String> firstPredicateKeywordList = Collections.singletonList("first");
+        List<String> secondPredicateKeywordList = Arrays.asList("first", "second");
+
+        TagListContainsKeywordsPredicate firstPredicate =
+                new TagListContainsKeywordsPredicate(firstPredicateKeywordList);
+        TagListContainsKeywordsPredicate  secondPredicate =
+                new TagListContainsKeywordsPredicate(secondPredicateKeywordList);
+
+        // same object -> returns true
+        assertTrue(firstPredicate.equals(firstPredicate));
+
+        // same values -> returns true
+        TagListContainsKeywordsPredicate firstPredicateCopy =
+                new TagListContainsKeywordsPredicate(firstPredicateKeywordList);
+        assertTrue(firstPredicate.equals(firstPredicateCopy));
+
+        // different types -> returns false
+        assertFalse(firstPredicate.equals(1));
+
+        // null -> returns false
+        assertFalse(firstPredicate.equals(null));
+
+        // different person -> returns false
+        assertFalse(firstPredicate.equals(secondPredicate));
+    }
 }
