4cfdb36eb9059e60a1eed0e4a48a825ad0e9f10b
Nadhira Salsabila <nadhira_salsabila@yahoo.co.id>
2017-10-16 16:13:35 +0800

    Add BirthdayTest.java to test birthday field

---
+++ src/test/java/seedu/address/model/person/BirthdayTest.java
@@ -0,0 +1,27 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BirthdayTest {
+
+    @Test
+    public void isValidPhone() {
+        // invalid phone numbers
+        assertFalse(Birthday.isValidBirthday("")); // empty string
+        assertFalse(Birthday.isValidBirthday(" ")); // spaces only
+        assertFalse(Birthday.isValidBirthday("912345")); // less than 8 numbers
+        assertFalse(Birthday.isValidBirthday("phone")); // non-numeric
+        assertFalse(Birthday.isValidBirthday("9011p041")); // alphabets within digits
+        assertFalse(Birthday.isValidBirthday("9011@041")); // characters within digits
+        assertFalse(Birthday.isValidBirthday("9312 1534")); // spaces within digits
+        assertFalse(Birthday.isValidBirthday("123456789123456789")); // more than 8 numbers
+
+        // valid phone numbers
+        assertTrue(Birthday.isValidBirthday("01011991")); // exactly 8 numbers valid birthday
+        assertTrue(Birthday.isValidBirthday("99999999")); // exactly 8 numbers invalid birthday
+        assertTrue(Birthday.isValidBirthday("12345678")); // exactly 8 numbers invalid birthday
+    }
+}
