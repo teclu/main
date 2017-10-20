4ab145acce545872bf859b17c29fc415c9060a08
Nadhira Salsabila <nadhira_salsabila@yahoo.co.id>
2017-10-20 08:01:24 +0800

    Update logic in AddTagCommand

--- src/main/java/seedu/address/logic/commands/AddTagCommand.java
+++ src/main/java/seedu/address/logic/commands/AddTagCommand.java
@@ -1,12 +1,28 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.DuplicateDataException;
+import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Birthday;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
 import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
 
 /**
  * Adds a tag to a person in the address book.
@@ -17,11 +33,14 @@ public class AddTagCommand extends UndoableCommand {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a tag to a person in the address book. "
             + "Parameters: INDEX (must be a positive integer) "
-            + "TAG \n"
+            + "[TAG] \n"
             + "Example: " + COMMAND_WORD + " 1 "
-            + "owesMoney";
+            + "neighbours";
 
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Tag: %2$s";
+    public static final String MESSAGE_ADD_TAG_SUCCESS = "Tag added! " + MESSAGE_ARGUMENTS;
+    public static final String MESSAGE_DUPLICATE_TAG = "This tag already exists for this person.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person is already in the address book.";
 
     private final Index index;
     private final Set<Tag> addedTag;
@@ -41,7 +60,49 @@ public class AddTagCommand extends UndoableCommand {
 
     @Override
     public CommandResult executeUndoableCommand() throws CommandException {
-        throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), addedTag));
+        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        ReadOnlyPerson personToEdit = lastShownList.get(index.getZeroBased());
+        Person editedPerson = createEditedPerson(personToEdit, addedTag);
+
+        try {
+            model.updatePerson(personToEdit, editedPerson);
+        } catch (DuplicatePersonException dpe) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        } catch (PersonNotFoundException pnfe) {
+            throw new AssertionError("The target person cannot be missing");
+        }
+        model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(String.format(MESSAGE_ADD_TAG_SUCCESS, editedPerson));
+    }
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editPersonDescriptor}.
+     */
+    private static Person createEditedPerson(ReadOnlyPerson personToEdit,
+                                             Set<Tag> addedTag) throws CommandException{
+        assert personToEdit != null;
+
+        Name name = personToEdit.getName();
+        Phone phone = personToEdit.getPhone();
+        Email email = personToEdit.getEmail();
+        Address address = personToEdit.getAddress();
+        Birthday birthday = personToEdit.getBirthday();
+        
+        Set<Tag> initialTags = personToEdit.getTags();
+        Set<Tag> updatedTags = new HashSet<>();
+        
+        try {
+            updatedTags = getUpdatedTags(initialTags, addedTag);
+        } catch (DuplicateTagException e) {
+            throw new CommandException(e.getMessage());
+        }
+
+        return new Person(name, phone, email, address, birthday, updatedTags);
     }
 
     @Override
@@ -60,5 +121,30 @@ public class AddTagCommand extends UndoableCommand {
         AddTagCommand e = (AddTagCommand) other;
         return index.equals(e.index)
                 && addedTag.equals(e.addedTag);
+    }
+    
+    public static HashSet<Tag> getUpdatedTags(Set<Tag> initialTag, Set<Tag> addedTag) throws DuplicateTagException{
+        HashSet<Tag> updatedTags = new HashSet<>(initialTag);
+        for (Tag toAdd : addedTag) {
+            requireNonNull(toAdd);
+            if (initialTag.contains(toAdd)) {
+                throw new DuplicateTagException(toAdd.tagName);
+            }
+            updatedTags.add(toAdd);
+        }
+        return updatedTags;
+    }
+
+    /**
+     * Signals that an operation would have violated the 'no duplicates' property of the list.
+     */
+    public static class DuplicateTagException extends DuplicateDataException {
+        protected DuplicateTagException(String tagName) {
+            super(MESSAGE_DUPLICATE_TAG + tagName);
+        }
+        
+        public String getMessage() {
+            return super.getMessage();
+        }
     }
 }
