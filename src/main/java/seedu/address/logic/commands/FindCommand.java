30aff673d59a4d9ab71565cb48b813d827aadbf2
k-l-a <k.l.a@u.nus.edu>
2017-10-25 09:01:37 +0800

    Update add command message usage, and find command for previous updates

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -15,7 +15,7 @@ public class FindCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names or tags, depending "
             + "on an optional given prefix, contain any of the specified keywords (case-insensitive) and displays "
-            + "them as a list with index numbers. Matches name by default.\n"
+            + "them as a list, sorted by the position of matched string, with index numbers. Matches name by default.\n"
             + "Parameters: [PREFIX] KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " n/ alice bob charlie";
 
