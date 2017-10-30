13274084062b900835fd2100c3fc03b3396e8f03
Nadhira Salsabila <nadhira_salsabila@yahoo.co.id>
2017-10-30 14:42:15 +0800

    Create EmailContainsKeywordsPredicate to enable find by email

---
+++ src/main/java/seedu/address/model/person/EmailContainsKeywordsPredicate.java
@@ -0,0 +1,38 @@
+package seedu.address.model.person;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+
+import java.util.Comparator;
+import java.util.List;
+
+import seedu.address.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code ReadOnlyPerson}'s {@code Email} matches any of the keywords given.
+ */
+public class EmailContainsKeywordsPredicate extends FieldContainsKeywordsPredicate {
+    private final List<String> keywords;
+
+    public EmailContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+        this.fieldToSearch = PREFIX_EMAIL.getPrefix();
+    }
+
+    @Override
+    public boolean test(ReadOnlyPerson person) {
+        return keywords.stream().anyMatch(keyword -> StringUtil
+                .containsWordIgnoreCase(person.getEmail().value, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof EmailContainsKeywordsPredicate // instanceof handles nulls
+                && this.keywords.equals(((EmailContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+    @Override
+    public Comparator<ReadOnlyPerson> sortOrderComparator() {
+        return (person1, person2) -> (0); //no sorting for email
+    }
+}
