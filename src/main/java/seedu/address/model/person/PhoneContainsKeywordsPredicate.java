cc5e635982252f2ab706e01bd3ea3add016a3e8a
Nadhira Salsabila <nadhira_salsabila@yahoo.co.id>
2017-10-30 14:25:11 +0800

    Create PhoneContainsKeywordsPredicate to enable find by phone

---
+++ src/main/java/seedu/address/model/person/PhoneContainsKeywordsPredicate.java
@@ -0,0 +1,38 @@
+package seedu.address.model.person;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+
+import java.util.Comparator;
+import java.util.List;
+
+import seedu.address.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code ReadOnlyPerson}'s {@code Phone} matches any of the keywords given.
+ */
+public class PhoneContainsKeywordsPredicate extends FieldContainsKeywordsPredicate {
+    private final List<String> keywords;
+
+    public PhoneContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+        this.fieldToSearch = PREFIX_PHONE.getPrefix();
+    }
+
+    @Override
+    public boolean test(ReadOnlyPerson person) {
+        return keywords.stream().anyMatch(keyword -> StringUtil
+                .containsWordIgnoreCase(person.getPhone().value, keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof PhoneContainsKeywordsPredicate // instanceof handles nulls
+                && this.keywords.equals(((PhoneContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+    @Override
+    public Comparator<ReadOnlyPerson> sortOrderComparator() {
+        return (person1, person2) -> (0); //no sorting for phone
+    }
+}
