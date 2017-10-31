a5aa291dd17e41d51b91fe745edbb2aeb091a21c
k-l-a <k.l.a@u.nus.edu>
2017-11-01 03:17:35 +0800

    Let addressbook parser return sortcommandparser in case of sortcommand's command word

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -82,7 +82,7 @@ public class AddressBookParser {
             return new FindCommandParser().parse(arguments);
 
         case SortCommand.COMMAND_WORD:
-            return null;
+            return new SortCommandParser().parse(arguments);
 
         case ListCommand.COMMAND_WORD:
         case ListCommand.COMMAND_ALIAS:
