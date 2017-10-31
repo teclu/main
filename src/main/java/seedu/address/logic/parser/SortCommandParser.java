7ed93239b4fdc56c4fc4a9849bfbdb85884767d5
k-l-a <k.l.a@u.nus.edu>
2017-11-01 03:10:04 +0800

    Create isSortablePrefix method in SortCommandParser

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -1,4 +1,14 @@
 package seedu.address.logic.parser;
 
 public class SortCommandParser {
+
+    /**
+     * Checks if the given string prefix is a sortable prefix (n/, p/, e/, a/, b/).
+     */
+    public boolean isSortablePrefix(String prefixString) {
+        return prefixString.equals(PREFIX_NAME.getPrefix()) || prefixString.equals(PREFIX_PHONE.getPrefix())
+                || prefixString.equals(PREFIX_ADDRESS.getPrefix()) || prefixString.equals(PREFIX_EMAIL.getPrefix())
+                || prefixString.equals(PREFIX_BIRTHDAY.getPrefix());
+    }
+
 }
