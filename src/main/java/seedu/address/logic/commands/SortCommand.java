848936745a723cca5cba7bf0ecc2ff89d46c4fef
k-l-a <k.l.a@u.nus.edu>
2017-11-01 06:28:22 +0800

    Let sortcommand use a variable instead of null for default

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.commands;
 
-import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_BIRTHDAY;
@@ -23,10 +22,10 @@ public class SortCommand extends Command {
 
     public static final String ARGUMENT_ASCENDING_WORD = "asc";
     public static final String ARGUMENT_DESCENDING_WORD = "des";
-
+    public static final String ARGUMENT_DEFAULT_ORDER = "default";
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Sorts either the current list according to"
-            + "given arguments. No prefix results in default order.\n"
+            + ": Sorts the current list according to given prefix in the given order. "
+            + "No prefix results in default order.\n"
             + "Parameters: [PREFIX] [ORDER]\n"
             + "Example : sort n/ asc";
     public static final String MESSAGE_SORT_SUCCESS = "Sorted current list by %1$s in %2$s order.";
@@ -42,29 +41,20 @@ public class SortCommand extends Command {
     @Override
     public CommandResult execute() throws CommandException {
         Comparator<ReadOnlyPerson> sortOrder = createAscComparator(prefix, order);
-
-        String prefix_string = prefix;
-        String order_string = order;
-        if (isNull(order)) {
-            order_string = "asc";
-        }
-        if (isNull(prefix)) {
-            prefix_string = "default";
-        }
         model.updateSortedFilteredPersonList(null); //reset any order first
         model.updateSortedFilteredPersonList(sortOrder);
-        return new CommandResult(String.format(MESSAGE_SORT_SUCCESS, prefix_string, order_string));
+        return new CommandResult(String.format(MESSAGE_SORT_SUCCESS, prefix, order));
     }
 
     /**
      *  Creates a ReadOnlyPerson Comparator from the given prefix and ordering.
      */
     public Comparator<ReadOnlyPerson> createAscComparator(String prefix, String order) {
-        if (isNull(prefix)) {
+        if (prefix.equals(ARGUMENT_DEFAULT_ORDER)) {
             if (ARGUMENT_DESCENDING_WORD.equals(order)) {
-                return (person1, person2) -> (1);
+                return (person1, person2) -> (1); //reverse order
             } else if (ARGUMENT_ASCENDING_WORD.equals(order)) {
-                return (person1, person2) -> (-1);
+                return (person1, person2) -> (-1); //default order
             }
         }
 
@@ -91,5 +81,15 @@ public class SortCommand extends Command {
             sortOrderComp = sortOrderComp.reversed();
         }
         return sortOrderComp;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this
+                || (other instanceof SortCommand
+                && this.prefix.equals(((SortCommand) other).prefix)
+                && this.order.equals(((SortCommand) other).order));
+
+
     }
 }
