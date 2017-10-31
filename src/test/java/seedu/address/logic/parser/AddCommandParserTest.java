4d58cd9d9de660554290494716a40d1255dc6d8e
Stanley Tay <saintpatricktay@gmail.com>
2017-11-01 01:15:33 +0800

    Restore previous AddCommandParserTest.java

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -104,36 +104,6 @@ public class AddCommandParserTest {
                 .withAvatar(VALID_AVATAR_AMY).withTags().build();
         assertParseSuccess(parser, AddCommand.COMMAND_WORD + NAME_DESC_AMY + PHONE_DESC_AMY + EMAIL_DESC_AMY
                 + BIRTHDAY_DESC_AMY + ADDRESS_DESC_AMY + AVATAR_DESC_AMY, new AddCommand(expectedPerson));
-
-        // no phone
-        expectedPerson = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(Phone.DEFAULT_VALUE)
-                .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withBirthday(VALID_BIRTHDAY_BOB)
-                .withAvatar(VALID_AVATAR_BOB).withTags(VALID_TAG_FRIEND).build();
-        assertParseSuccess(parser, AddCommand.COMMAND_WORD + NAME_DESC_BOB
-                + EMAIL_DESC_BOB + ADDRESS_DESC_BOB + BIRTHDAY_DESC_BOB + AVATAR_DESC_BOB
-                + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
-
-        // no email
-        expectedPerson = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-                .withEmail(Email.DEFAULT_VALUE).withAddress(VALID_ADDRESS_BOB).withBirthday(VALID_BIRTHDAY_BOB)
-                .withAvatar(VALID_AVATAR_BOB).withTags(VALID_TAG_FRIEND).build();
-        assertParseSuccess(parser, AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_BOB + ADDRESS_DESC_BOB
-                + BIRTHDAY_DESC_BOB + AVATAR_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
-
-        // no address
-        expectedPerson = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-                .withEmail(VALID_EMAIL_BOB).withAddress(Address.DEFAULT_VALUE).withBirthday(VALID_BIRTHDAY_BOB)
-                .withAvatar(VALID_AVATAR_BOB).withTags(VALID_TAG_FRIEND).build();
-        assertParseSuccess(parser, AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + BIRTHDAY_DESC_BOB + AVATAR_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
-
-        // no birthday
-        expectedPerson = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
-                .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withBirthday(Birthday.DEFAULT_VALUE)
-                .withAvatar(VALID_AVATAR_BOB).withTags(VALID_TAG_FRIEND).build();
-        assertParseSuccess(parser, AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_BOB + EMAIL_DESC_BOB
-                + ADDRESS_DESC_BOB + AVATAR_DESC_BOB + TAG_DESC_FRIEND, new AddCommand(expectedPerson));
-
     }
 
     @Test
@@ -161,6 +131,10 @@ public class AddCommandParserTest {
         assertParseFailure(parser, AddCommand.COMMAND_WORD + NAME_DESC_BOB + PHONE_DESC_BOB
                 + EMAIL_DESC_BOB + ADDRESS_DESC_BOB + VALID_BIRTHDAY_BOB + AVATAR_DESC_BOB, expectedMessage);
         //@@author
+
+        // all prefixes missing
+        assertParseFailure(parser, AddCommand.COMMAND_WORD + VALID_NAME_BOB + VALID_PHONE_BOB
+                + VALID_EMAIL_BOB + VALID_ADDRESS_BOB + VALID_BIRTHDAY_BOB + VALID_AVATAR_BOB, expectedMessage);
     }
 
     @Test
