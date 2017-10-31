d2cdea94dd230e5f3bd956184e79f9f7ad6adc90
k-l-a <k.l.a@u.nus.edu>
2017-11-01 03:18:28 +0800

    Create constructor method for SortCommand

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -1,5 +1,29 @@
 package seedu.address.logic.commands;
 
 public class SortCommand extends Command {
-    
+
+    public static final String COMMAND_WORD = "sort";
+
+    public static final String OPTION_ALL_WORD = "-a";
+    public static final String ARGUMENT_ASCENDING_WORD = "asc";
+    public static final String ARGUMENT_DESCENDING_WORD = "des";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sorts either the current list according to"
+            + "given arguments. No prefix results in default order.\n"
+            + "Parameters: [" + OPTION_ALL_WORD + "] [PREFIX] [ORDER]\n"
+            + "Example : sort n/ asc";
+    public static final String MESSAGE_SORT_SUCCESS = "Sorted current list by %1$s in %2$s order.";
+    public static final String MESSAGE_SORT_ALL_SUCCESS = "Set default sort order to sort by %1$s in %2$s order.";
+
+    public final boolean sortAll;
+    public final String prefix;
+    public final String order;
+
+    public SortCommand(boolean sortAll, String prefix, String order) {
+        this.sortAll = sortAll;
+        this.prefix = prefix;
+        this.order = order;
+    }
+
 }
