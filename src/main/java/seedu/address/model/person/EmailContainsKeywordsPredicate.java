6f4d041c7cc1aa5a776486dae426f50856911129
k-l-a <k.l.a@u.nus.edu>
2017-11-07 01:38:43 +0800

    Change default sort order for predicates to null

--- src/main/java/seedu/address/model/person/EmailContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/EmailContainsKeywordsPredicate.java
@@ -34,6 +34,6 @@ public class EmailContainsKeywordsPredicate extends FieldContainsKeywordsPredica
 
     @Override
     public Comparator<ReadOnlyPerson> sortOrderComparator() {
-        return (person1, person2) -> (0); //no sorting for email
+        return defaultSortOrder; //no sorting for email
     }
 }
