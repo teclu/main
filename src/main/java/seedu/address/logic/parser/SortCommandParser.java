0d45f72050bf1a6001ea8e54e523576f8b502f37
k-l-a <k.l.a@u.nus.edu>
2017-11-08 05:11:04 +0800

    Update header comments

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -22,8 +22,8 @@ public class SortCommandParser implements Parser<SortCommand> {
 
     /**
      * Parses the given {@code String} of arguments in the context of the SortCommand
-     * and returns an SortCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
+     * @return SortCommand object for execution.
      */
     public SortCommand parse(String args) throws ParseException {
         requireNonNull(args);
