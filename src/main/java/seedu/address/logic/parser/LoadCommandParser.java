b0172e58e1d140e274af4a4de4026045e3fa06c5
k-l-a <k.l.a@u.nus.edu>
2017-11-08 05:07:55 +0800

    Implement the parse method in LoadCommandParser

--- src/main/java/seedu/address/logic/parser/LoadCommandParser.java
+++ src/main/java/seedu/address/logic/parser/LoadCommandParser.java
@@ -1,4 +1,30 @@
 package seedu.address.logic.parser;
 
-public class LoadCommandParser {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.LoadCommand.MESSAGE_USAGE;
+
+import seedu.address.logic.commands.LoadCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and create a new LoadCommand Object.
+ */
+public class LoadCommandParser implements Parser<LoadCommand> {
+
+    /**
+     * Parses the given (@code String) in the context of a LoadCommand.
+     * @param args
+     * @return LoadCommand Object for execution
+     */
+    public LoadCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, MESSAGE_USAGE));
+        }
+
+        return new LoadCommand(trimmedArgs);
+    }
 }
