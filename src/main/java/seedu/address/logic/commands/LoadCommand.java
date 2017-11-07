50e1e9ab361281df8f3abc117c07bfd114051fe3
k-l-a <k.l.a@u.nus.edu>
2017-11-08 05:09:08 +0800

    Declare static variables in LoadCommand

--- src/main/java/seedu/address/logic/commands/LoadCommand.java
+++ src/main/java/seedu/address/logic/commands/LoadCommand.java
@@ -1,4 +1,19 @@
 package seedu.address.logic.commands;
 
 public class LoadCommand {
+/**
+ * Loads the address book on the given filepath
+ */
+public class LoadCommand extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "load";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Loads the address book on the given filepath and overwrite the current address book.\n"
+            + "Parameters: FILEPATH\n"
+            + "Example: load data/addressbook-backup.xml";
+
+    public static final String MESSAGE_LOAD_SUCCESS = "Load successful! File loaded from %1$s";
+    public static final String MESSAGE_FILE_NOT_FOUND = "File not found at %1$s";
+
 }
