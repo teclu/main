862f8e97d0d8383969c8085a645d4b73ac2dbb63
k-l-a <k.l.a@u.nus.edu>
2017-11-12 15:31:29 +0800

    Declare static variables

--- src/test/java/seedu/address/logic/parser/ImportCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ImportCommandParserTest.java
@@ -1,4 +1,15 @@
 package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import org.junit.Test;
+import seedu.address.logic.commands.ImportCommand;
+
 public class ImportCommandParserTest {
+    private static final String MESSAGE_INVALID_FORMAT = String
+            .format(MESSAGE_INVALID_COMMAND_FORMAT, ImportCommand.MESSAGE_USAGE);
+    private ImportCommandParser parser = new ImportCommandParser();
+
+    @Test
+    public void parse_empty
 }
